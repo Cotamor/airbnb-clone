816a84cfdaa2642dafb4914f86ee78d165b439b0
Cotamor <cotamor@gmail.com>
2023-07-18 21:30:42 +0900

    google and github signin

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com']
-  }
+    domains: [
+      'images.pexels.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+    ],
+  },
 }
 
 module.exports = nextConfig
