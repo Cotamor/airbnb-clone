73941dc4fe34b9959b728d509ccedad1f5fce29a
Cotamor <cotamor@gmail.com>
2023-07-23 01:50:06 +0900

    favorites page

--- app/(routes)/favorites/page.tsx
+++ app/(routes)/favorites/page.tsx
@@ -1,6 +1,30 @@
-const FavoritesPage = () => {
+import getCurrentUser from '@/actions/get-current-user'
+import getFavoriteListings from '@/actions/get-favorite-listings'
+import ClientOnly from '@/components/client-only'
+import EmptyState from '@/components/empty-state'
+import FavoritesClient from './components/favorites-client'
+
+const FavoritesPage = async () => {
+  const listings = await getFavoriteListings()
+  const currentUser = await getCurrentUser()
+
+  if(listings.length === 0) {
+
+    <ClientOnly>
+      <EmptyState
+        title="No favorites found"
+        subtitle="Looks like you have no favorite listings."
+      />
+    </ClientOnly>
+  }
+
   return (
-    <div>FavoritesPage</div>
+    <ClientOnly>
+      <FavoritesClient
+        listings={listings}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
   )
 }
 export default FavoritesPage
